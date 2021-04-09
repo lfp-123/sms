d28e04ff461ea5cf2ab6d576314dd6379d55144f
linfengpeng <1406051995@qq.com>
2021-04-09 09:41:04 +0800

    超市管理系统1.0

---
+++ SupermarketManager/src/test/java/DBTest.java
@@ -0,0 +1,18 @@
+import org.apache.ibatis.io.Resources;
+
+import java.io.IOException;
+
+/**
+ * @author ${¡÷∑Ê≈Ù}
+ * @Title: DBTest
+ * @ProjectName SupermarketManager
+ * @Description: TODO
+ * @date 2021/4/8 14:09
+ */
+public class DBTest {
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
