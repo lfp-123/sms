7ee3a2316ee2dd3d64261bb56a6630fb5bf8e70e
linfengpeng <1406051995@qq.com>
2021-04-12 08:57:55 +0800

    考勤管理实现代码冲突

--- SupermarketManager/src/main/java/com/supermarket/controller/EmpController.java
+++ SupermarketManager/src/main/java/com/supermarket/controller/EmpController.java
@@ -1,11 +1,14 @@
 package com.supermarket.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.supermarket.entity.Dept;
 import com.supermarket.entity.Employees;
 import com.supermarket.entity.EmployesWork;
 import com.supermarket.service.EmpService;
 import com.supermarket.util.ResponseUtil;
+import com.supermarket.util.StringUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/emp")
@@ -27,32 +28,50 @@ public class EmpController {
 
     @ResponseBody
     @RequestMapping("/empList")
-    public Map<String, Object> userList(Employees emp,
+    public Map<String, Object> userList(String empName,
                                         @RequestParam(value = "page", required = false) Integer page,
                                         @RequestParam(value = "limit", required = false) Integer limit) {
         Map<String, Object> result = ResponseUtil.resultFye(page, limit);
-        // 不知道为什么 这个有乱码 估计是roleInfo.jsp是复制的原因吧
-        if (emp.getEmpName() != null) {
-            String empName = new String(emp.getEmpName().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
-            result.put("empName", empName);
+        try {
+            Long count = empService.count(result);
+            if (StringUtil.isNotEmpty(empName)) {
+                return ResponseUtil.result(ResponseUtil.getEmpResultList(empService.findByEmpName(empName.trim()), empService), count);
+            } else {
+                return ResponseUtil.result(ResponseUtil.getEmpResultList(empService.findAll(result), empService), count);
+            }
+        } catch (Exception e) {
+            result.put("success", false);
+            result.put("errorInfo", "系统内部异常，查询失败，请联系管理员");
+            return result;
         }
-        System.out.println(emp.getEmpName());
-        List<Employees> roleList = empService.findAll(result);
-        Long count = empService.count(result);
-        return ResponseUtil.result(roleList, count);
+    }
+
+    @ResponseBody
+    @RequestMapping("/getDept")
+    public String deptList() {
+        List<Dept> deptList = empService.findAllDept();
+        return JSON.toJSONString(deptList);
     }
 
     @ResponseBody
     @RequestMapping("/add")
     public Map<String, Object> add(Employees emp) {
         Map<String, Object> result = new HashMap<>(2);
-        Employees empName = empService.findRepeat(emp.getEmpName());
-        if (empName == null) {
-            empService.add(emp);
-            result.put("success", true);
-        } else {
+        try {
+            Employees empName = empService.findRepeat(emp.getEmpName());
+            if (empName == null) {
+                if (empService.add(emp) > 0) {
+                    result.put("success", true);
+                } else {
+                    result.put("success", false);
+                }
+            } else {
+                result.put("success", false);
+                result.put("errorInfo", "用户名已存在！");
+            }
+        } catch (Exception e) {
             result.put("success", false);
-            result.put("errorInfo", "用户名已存在！");
+            result.put("errorInfo", "系统内部异常，增加失败，请联系管理员");
         }
         return result;
     }
@@ -61,8 +80,13 @@ public class EmpController {
     @RequestMapping("/delete")
     public Map<String, Object> delete(@RequestParam(value = "id", required = false) Integer id) {
         Map<String, Object> result = new HashMap<>(2);
-        empService.delete(id);
-        result.put("success", true);
+        try {
+            empService.delete(id);
+            result.put("success", true);
+        } catch (Exception e) {
+            result.put("success", false);
+            result.put("errorInfo", "系统内部异常，删除失败，请联系管理员");
+        }
         return result;
     }
 
@@ -70,9 +94,13 @@ public class EmpController {
     @RequestMapping("/deletew")
     public Map<String, Object> deletew(@RequestParam(value = "id", required = false) Integer id) {
         Map<String, Object> result = new HashMap<>(2);
-        empService.deletew(id);
-        System.out.println("Id+"+id);
-        result.put("success", true);
+        try {
+            empService.deleteew(id);
+            result.put("success", true);
+        } catch (Exception e) {
+            result.put("success", false);
+            result.put("errorInfo", "系统内部异常，删除失败，请联系管理员");
+        }
         return result;
     }
 
@@ -80,16 +108,27 @@ public class EmpController {
     @RequestMapping("/update")
     public Map<String, Object> update(Employees emp) {
         Map<String, Object> result = new HashMap<>(2);
-        empService.update(emp);
-        result.put("success", true);
+        try {
+            empService.update(emp);
+            result.put("success", true);
+        } catch (Exception e) {
+            result.put("success", false);
+            result.put("errorInfo", "系统内部异常，修改失败，请联系管理员");
+        }
         return result;
     }
+
     @ResponseBody
     @RequestMapping("/updatew")
     public Map<String, Object> updatew(EmployesWork emp) {
         Map<String, Object> result = new HashMap<>(2);
-        empService.updatew(emp);
-        result.put("success", true);
+        try {
+            empService.updatew(emp);
+            result.put("success", true);
+        } catch (Exception e) {
+            result.put("success", false);
+            result.put("errorInfo", "系统内部异常，修改失败，请联系管理员");
+        }
         return result;
     }
 
@@ -113,7 +152,7 @@ public class EmpController {
     public Map<String, Object> goodsList(@RequestParam(value = "page", required = false) Integer page,
                                          @RequestParam(value = "limit", required = false) Integer limit) {
         Map<String, Object> result = ResponseUtil.resultFye(page, limit);
-        System.out.println("page:"+page+"limit: "+limit);
+        System.out.println("page:" + page + "limit: " + limit);
         result.put("state", 2);
         List<EmployesWork> all = empService.findAllw(page, limit);
         Long count = empService.counts(result);
